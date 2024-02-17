206680b610f9a84a98d4c63a54ae7f6df4898080
CharanMV2913 <charankumarv2913@gmail.com>
2024-02-17 14:09:57 +0530

    compile.f file added

---
+++ compile.f
@@ -0,0 +1,6 @@
+/home/vlsi_S7/Desktop/CORDIC-VERILOG/Cordic.v
+/home/vlsi_S7/Desktop/CORDIC-VERILOG/CordicPipe.v
+/home/vlsi_S7/Desktop/CORDIC-VERILOG/CordicProc.v
+/home/vlsi_S7/Desktop/CORDIC-VERILOG/Post.v
+/home/vlsi_S7/Desktop/CORDIC-VERILOG/Pre.v
+
